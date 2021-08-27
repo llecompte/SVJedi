b20adb33c3a2d8d4677ec54be8357242db5a5364
llecompte <lolita.lecompte@gmail.com>
2021-08-27 14:58:47 +0200

    Update test data

--- Data/C_elegans/test.sh
+++ Data/C_elegans/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 ../../svjedi.py -v test.vcf -r genome.fasta -i simulated-reads.fastq.gz
+python3 ../../svjedi.py -v test.vcf -r reference.fasta -i simulated-reads.fastq.gz
 
 #Test if output as expected
-md5sum -c <<<"e0bcc73775f1b22921ea2ff29991af15 genotype_results.vcf"
+md5sum -c <<<"0157c1baf07daf604e71395284849834 genotype_results.vcf"
